064ad5600efee94ec7b023b4daaacd3c3a58a2bd
Katsunori Kuroda <99164438+nekodayo2222@users.noreply.github.com>
2024-10-18 13:44:07 +0900

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "off",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
